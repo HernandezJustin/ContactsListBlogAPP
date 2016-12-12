f276b26f70856d1e2f838080463a3b7cf2950da7
Justin Hernandez <kaspersky102@aol.com>
2016-12-12 17:52:55 -0500

    fixed controller skeleton to include changes and added appropriate view page, added code to index and create action, generated form for new action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,22 @@
 class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
-    @contact_count = Contact.count
   end
 
   def new
+  end
+
+  def create
+    @contact = Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      being_called?: false
+    )
+    redirect_to '/contacts'
+  end
+
+  def show
     # code goes here
   end
 
